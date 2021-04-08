3204edc62fd1fc0f62f326e6c1f8f2383dcd183d
devin mitchell <devin.a.mitchell@gmail.com>
2021-04-08 17:45:42 -0600

    finished MVP

--- GitHubCard/index.js
+++ GitHubCard/index.js
@@ -1,9 +1,23 @@
+import axios from 'axios' 
+
 /*
   STEP 1: using axios, send a GET request to the following URL
     (replacing the placeholder with your Github name):
     https://api.github.com/users/<your name>
 */
+const followersArray = ["tetondan", "dustinmyers", "justsml", "luishrd", "bigknell"];
+const cards = document.querySelector('.cards')
 
+followersArray.forEach((item) => {
+  axios.get(`https://api.github.com/users/${item}`)
+    .then(res => {
+      cards.appendChild(gitHubUser(res.data))
+    })
+    .catch(err => {
+      err = 'error'
+      console.log(err)
+    })
+})
 /*
   STEP 2: Inspect and study the data coming back, this is YOUR
     github info! You will need to understand the structure of this
@@ -28,7 +42,7 @@
     user, and adding that card to the DOM.
 */
 
-const followersArray = [];
+
 
 /*
   STEP 3: Create a function that accepts a single object as its only argument.
@@ -49,7 +63,52 @@ const followersArray = [];
       </div>
     </div>
 */
+function gitHubUser (userObj) {
+//instantiate
+const card = document.createElement('div')
+const userImg = document.createElement('img')
+const info = document.createElement('div')
+const name = document.createElement('h3')
+const userName = document.createElement('p')
+const location = document.createElement('p')
+const profile = document.createElement('p')
+const profileLink = document.createElement('a')
+const followers = document.createElement('p')
+const following = document.createElement('p')
+const bio = document.createElement('p')
 
+//structure
+card.appendChild(userImg)
+card.appendChild(info)
+info.appendChild(name)
+info.appendChild(userName)
+info.appendChild(location)
+info.appendChild(profile)
+info.appendChild(followers)
+info.appendChild(following)
+info.appendChild(bio)
+profile.appendChild(profileLink)
+
+//add class
+card.classList.add('card')
+info.classList.add('car-info')
+name.classList.add('name')
+userName.classList.add('username')
+
+
+//textContent
+userImg.src = userObj.avatar_url
+name.textContent = userObj.name
+userName.textContent = userObj.login
+location.textContent = userObj.location
+profileLink.textContent = userObj.url
+followers.textContent = userObj.followers
+following.textContent = userObj.following 
+bio.textContent = userObj.bio
+
+return card;
+}
+// console.log(gitHubUser())
 /*
   List of LS Instructors Github username's:
     tetondan
